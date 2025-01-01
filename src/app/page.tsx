09f122f8df572abc9ad0cc0b3c62e92a1acfeb27
Basit ali <160397104+Basit1478@users.noreply.github.com>
2025-01-01 23:29:35 +0500

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,25 @@
+
+import Footer from '@/components/Footer'
+import HeroSection from '@/components/HeroSection'
+import Navbar from '@/components/Navbar'
+import React from 'react'
+import Blog from '@/components/Blog'
+const page = () => {
+  return (
+    <div > 
+<main>
+  <Navbar/>
+  <HeroSection/>
+<Blog/>
+  <Footer/>
+</main>
+
+    </div>
+  )
+}
+
+
+
+  
+
+export default page
