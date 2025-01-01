c3f8826395ba877c4cba300eb67413d2ae8b757b
Basit ali <160397104+Basit1478@users.noreply.github.com>
2025-01-02 00:09:53 +0500

    Create page.tsx

---
+++ src/app/informative/top-5-online-platforms/page.tsx
@@ -0,0 +1,302 @@
+import CommentSection from '@/components/CommentSection'
+import Footer from '@/components/Footer'
+import Navbar from '@/components/Navbar'
+import React from 'react'
+import Image from 'next/image'
+const page = () => {
+  return (
+    <div>
+        <Navbar/>
+        <div>
+       <div className="min-h-screen p-6 bg-gradient-to-r from-gray-50 to-gray-100">
+    <div className= "max-w-4xl mx-auto bg-gray-50 shadow-lg rounded-lg p-8">
+   <Image
+  alt="Background image"
+  className="w-full h-auto rounded-md shadow-md mb-6"
+  width={800}
+  height={400}
+  loading="eager"
+  role="presentation"
+  src="/images/pic8.jpg"
+/>
+      <p className="text-lg">
+
+     <span className='text-2xl font-bold' >1. GetResponse</span>
+      <br />
+      <br />
+      Beyond being your go-to email marketing tool, GetResponse offers a dedicated knowledge 
+      base with a few digital marketing certification courses. With 14 renowned digital marketing 
+      experts, you’ll access rich courses to improve your online marketing game. 
+      <br />
+      <br />
+You’ll learn from the likes of Andrew Davis, Kath Paym, Michael Brenner, and Jamie Turner.
+<br />
+<br />
+Some major courses on GetResponse include marketing automation, content marketing, landing page and 
+conversion optimization, email marketing, and inbound marketing. 
+<br />
+<br />
+These in-depth courses provide a hands-on learning experience. You’ll learn based on real-life case
+ studies and best practices.
+ <br />
+ <br />
+Apart from these certification courses, GetResponse also offers a pool of resources to solidify your 
+knowledge of email marketing. You’ll find ebooks, webinars, and free video tutorials.
+<br />
+<br />
+<span className='text-2xl font-bold'>Pros</span>
+<br />
+<br />
+Learn from expert instructors in each course <br />
+Courses are thorough <br />
+Engaging learning experience <br />
+<br />
+<span className='text-2xl font-bold'>Cons</span>
+<br />
+<br />
+GetResponse courses focus on digital marketing alone
+<br />
+<br />
+<span className='text-2xl font-bold'>GetResponse Prices</span>
+<br />
+<br />
+At the moment, the GetResponse courses are available completely free!
+<br />
+<br />
+GetResponse, however, isn’t only a learning platform but also a digital marketing and content monetization 
+platform. This means that besides giving you the opportunity to learn new things, it enables you to create
+ and market your own courses.
+<br />
+<br />
+<span className='text-2xl font-bold'> 2. SkillShare</span>
+<br />
+<br />
+SkillShare is an online course platform with a rich library of classes taught by industry experts on a 
+wide range of creative topics. It’s not as formal as other learning platforms, but you’re 
+likely to enjoy the practicality of Skillshare courses.
+<br />
+<br />
+Whether you dream of mastering animation or brushing up your photography skills, 
+Skillshare has something to help you get started. You only need to browse various
+ categories like illustrations, marketing, or even crafts. Most classes also offer
+  downloadable resources so you can learn on the go.
+<br />
+<br />
+One thing that makes Skillshare remarkable is its focus on project-based learning. 
+You’ll find courses by instructors who are willing to guide you through your own
+ creative process as you hone your skills.
+<br />
+<br />
+<span className='text-2xl font-bold'>Pros</span>
+<br />
+<br /> 
+User-friendly platform <br />
+Wide range of courses for creatives <br />
+Content is community-driven <br />
+Courses are affordable <br />
+Courses are short and straightforward <br />
+<br />
+<br />
+<span className='text-2xl font-bold'>Cons</span>
+<br />
+<br />
+Not a lot of technical or academic courses <br />
+Some instructors may not be trained professionals <br />
+Not all courses have the best quality <br />
+<br />
+<br />
+<span className='text-2xl font-bold'>Skillshare pricing</span>
+<br />
+<br />
+While there’s a free trial, you may need a premium membership to access the entire library and enticing
+ features like digital downloads and community discussions. However, you can access a few introductory 
+ courses without a subscription. 
+<br />
+<br />
+The pricing mostly depends on your country of residence. On average, you may pay up to $168 yearly 
+or $14 monthly.
+<br />
+<br />
+Overall, Skillshare is a platform worth trying if you want to improve your creativity or develop 
+skills that align with your passion.
+<br />
+<br />
+<span className='text-2xl font-bold'>3. 360Training</span>
+<br />
+<br />
+360Training is another incredible option on this list of the best online learning platforms. 
+360Training focuses on providing industry-specific training and certification programs. 
+<br />
+<br />
+The e-learning platform specializes in compliance training and professional development courses. 
+So, if you’re looking to upskill for a new career or fulfill licensing requirements, 360Training 
+has the right courses for you. 
+<br />
+<br />
+360Training stands out because it focuses on industry-recognized credentials and regulatory compliance.
+ Their course library features a comprehensive selection covering topics like OSHA safety standards, 
+ real estate licensing, and other professional development programs for various sectors.
+
+360Training also offers a Learning Management System (LMS) that companies can use to streamline employee 
+training. The LMS provides a centralized platform for course enrolment, progress tracking, and reporting. 
+<br />
+<br />
+<span className='text-2xl font-bold'>Pros</span>
+<br />
+<br />
+The platform is straightforward to navigate <br />
+Offers self-paced learning <br />
+It’s rich with certification programs <br />
+Credentials are widely recognized <br />
+<br />
+<span className='text-2xl font-bold'>Cons</span>
+<br />
+<br />
+Limited variety of topics, unlike other online course platforms <br />
+The quality of courses may vary <br />
+<br />
+<span className='text-2xl font-bold'>360Training pricing</span>
+<br />
+<br />
+360Training sell online courses as individual course purchases or subscription plans. 
+You can go for either, depending on your needs. Individual course prices also depend on 
+the topic and duration. You’ll find courses from as low as $8 to up to $180. You can also 
+purchase courses in bulk and get discounts.
+<br />
+<br />
+<span className='text-2xl font-bold'>4. Coursera</span>
+<br />
+<br />
+Coursera provides access to learning programs and courses created and presented by 
+instructors from over 300 world-class universities and companies. The learning platform 
+partners with top universities and industry leaders like Yale, University of Michigan,
+ Cambridge, Google, and IBM.
+<br />
+<br />
+Coursera also offers over 5,000 programs that you can complete within a day. 
+<br />
+<br />
+Coursera offers a diverse range of courses. You can choose from specialized (multi-course) 
+programs, courses with professional certificates, and even full-fledged degree programs. 
+<br />
+<br />
+So, whether you’re looking for a degree in computer science from a top university or a data 
+analysis course with a tech company, there’s a course for you.
+<br />
+<br />
+Courses on Coursera incorporate interactive elements like quizzes, video lectures, and graded 
+assignments. So, you can trust your learning experience to be engaging.
+<br />
+<br />
+Coursera is quite user-friendly. You can browse courses by subject, skill level, or even the 
+university/company that offers these courses. There’s also a mobile app you can use to access
+ courses on the go.
+<br />
+<br />
+<span className='text-2xl font-bold'>Pros</span>
+<br />
+<br />
+Extensive library with a wide range of topics <br />
+Collaborates with top universities and industry leaders <br />
+Offers various learning options. There are short skill-based courses and more in-depth certification programs <br />
+<br />
+<span className='text-2xl font-bold'>Cons</span>
+<br />
+<br />
+Can be time-consuming to complete longer courses <br />
+Degree certifications are quite expensive <br />
+Free courses don’t come with certificates <br />
+Some courses have time limits which may not be realistic for everyone <br />
+<br />
+<span className='text-2xl font-bold'>Coursera pricing</span>
+<br />
+<br />
+Prices vary depending on the type of program you choose. Individual courses often come with a 
+subscription fee. You’ll have three subscription plans to choose from:
+<br />
+<br />
+The Single learning program at $48-$79 monthly <br />
+Coursera plus monthly subscription at $59 monthly, and <br />
+Coursera plus annual at $399 yearly with a money-back guarantee. <br />
+<br />
+Companies can access the Coursera for Teams subscription for 5 to125 Users at $399 per user/year.
+<br />
+<br />
+Specializations and professional certificates may require a one-time fee. Coursera degrees 
+are usually more expensive if you’re seeking a more substantial qualification. However, they 
+can be instrumental in advancing your career. Also, Coursera provides financial aid options 
+(these often come in the form of discounts) to make certification programs more accessible.
+<br />
+<br />
+<span className='text-2xl font-bold'>5. edX</span>
+<br />
+<br />
+edX started as a free, non-profit online learning platform. It was created as a 
+joint venture by industry leaders and NGOs, as well as various reputable universities 
+like MIT, Wharton, Boston, and Harvard. 
+<br />
+<br />
+Today, edX has over 260 content partners and offers over 4,000 courses. edX’s library 
+has a wide range of courses designed to expand your understanding and equip you with valuable skills.
+<br />
+<br />
+The platform focuses on high-quality academic content. It offers a rich blend of courses 
+ranging from introductory lectures to advanced programs across different disciplines. 
+You’ll find classes like marketing, artificial intelligence, cybersecurity, and more. 
+You can bet that the courses you access on edX are well-rounded and thorough. 
+<br />
+<br />
+Additionally, edX offers MicroMasters programs, a series of intensive courses designed
+ to provide a specialized qualification. There are also full degree programs from partner universities.
+<br />
+<br />
+Similar to other platforms, edX prioritizes user-friendliness. The site’s search 
+bar allows users to find specific courses based on subject, institution, or skill level. 
+<br />
+<br />
+<span className='text-2xl font-bold'>Pros</span>
+<br />
+<br />
+Courses are backed by renowned institutions <br />
+Micromasters and professional certificates are available <br />
+Partners with top institutions and industry leaders <br />
+Offers top-notch learning resources <br />
+<br />
+<br />
+<span className='text-2xl font-bold'>Cons</span>
+<br />
+<br />
+Some courses may be too complex to follow <br />
+You may not find a lot of courses for creatives <br />
+Verified certificates may be too expensive <br />
+<br />
+<br />
+<span className='text-2xl font-bold'>edX pricing</span>
+<br />
+<br />
+edX has loads of open courses which you can take at no cost. These courses do not 
+offer certificates or give you access to graded assignments. However, the prices for
+ certifications depend on the course. Expect to pay between $50 and $300 for individual courses. 
+<br />
+<br />
+Additionally, courses within categories like professional education, boot camps, and 
+executive education may have higher prices. 
+<br />
+<br />
+edX also has a subscription plan for business. This plan is designed for 30 learners 
+or less and is offered at $349 per learner. There is also a custom plan for larger organizations.
+
+
+
+      </p>
+     
+      <CommentSection/>
+    </div>
+    </div>
+    </div>
+    <Footer/>
+    </div>
+  )
+}
+
+export default page
