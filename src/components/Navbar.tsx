7182025ddc8bedea3dca951730823ee9dc49df83
Basit ali <160397104+Basit1478@users.noreply.github.com>
2025-01-01 23:59:20 +0500

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -65,7 +65,7 @@ const Navbar = () => {
           <div className="md:hidden">
             <button
               onClick={toggleMenu}
-              className="text-gray-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-white"
+              className="text-orange-500 hover:text-orange-700 focus:outline-none focus:ring-2 focus:ring-orange-800"
             >
               <svg
                 className="h-6 w-6"
@@ -95,13 +95,13 @@ const Navbar = () => {
         <div className="md:hidden bg-white">
           <div className="px-2 pt-2 pb-3 space-y-1">
             <a
-              href="#"
+              href="/"
               className="block px-3 py-2 rounded-md text-base font-medium hover:text-orange-600"
             >
               Home
             </a>
             <a
-              href="#"
+              href="/about"
               className="block px-3 py-2 rounded-md text-base font-medium hover:text-orange-600"
             >
               About
